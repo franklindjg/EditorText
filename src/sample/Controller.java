cee8467b75cd447c45f8d138a6dcb4e40b89b48d
Franklin De Jesus Gomez <26652720k@iespoblenou.org>
2016-10-11 00:03:49 +0200

    Importando classes que necesito para no cargar todas las clases de import java.io.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,8 +8,11 @@ import javafx.scene.control.*;
 import javafx.scene.text.Font;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
-import java.io.*;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileReader;
 
 public class Controller {
 
