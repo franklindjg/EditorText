3060713eb9a8f5ca779450aabfee855cf873675c
Franklin De Jesus Gomez <26652720k@iespoblenou.org>
2016-10-12 12:18:08 +0200

    Cambiando dimensión de la ventana

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
         primaryStage.setTitle("Text editor");
-        primaryStage.setScene(new Scene(root, 700, 500));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
